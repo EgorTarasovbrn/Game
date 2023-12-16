7d1ff0d0e17e5d05dea10397fa5b4d934fee8559
Egor <EgorTarasovbrn@yandex.ru>
2023-12-16 21:56:46 +0700

    1.Создал класс персонажа 2.Добавил прыжок 3.Скролл экрана 4.Класс платформ 5.бесконечное добавление платформ 6.экран с game over, если игрок упал 7.нарисовал игрока 8.нарисовал платформу

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ screen = pygame.display.set_mode((SCREEN_WINDTH, SCREEN_HEIGHT))
 # название окна
 pygame.display.set_caption('Game')
 
-# Загрузка изображения(просто для примера)
-image_background = pygame.image.load('game_fon.jpg')  # задний фон
+# Загрузка изображения
 image_platform = pygame.image.load('data/platform.png')  # платформа
 
 image_person = pygame.image.load('data\player.png')  # игрок
@@ -62,23 +61,51 @@ class Player(pygame.sprite.Sprite):
             self.rect.x = SCREEN_WINDTH
         elif self.rect.x > SCREEN_WINDTH:
             self.rect.x = -self.rect.width
+
         # проверка запрыгнул ли игрок на платформу
         for platform in sprite_platforms:
+            # если при прыжке будет пересечение, то...
             if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.rect.width, self.rect.height):
+                # если игрок находится выше платформы
                 if self.rect.bottom < platform.rect.centery:
                     if self.vel_y > 0:
                         self.rect.bottom = platform.rect.top
                         dy = 0
                         self.vel_y = -20
+
         # проверка коснулся ли игрок скролла или нет
         if self.rect.y <= SCROLL_TRIGGER and self.vel_y < 0:
             scroll = -dy
 
-        self.rect.x += dx # изменение координаты х у игрока(движение)
-        self.rect.y += dy + scroll # изменение координаты y у игрока(движение)
+        self.rect.x += dx  # изменение координаты х у игрока(движение)
+        self.rect.y += dy + scroll  # изменение координаты y у игрока(движение)
         return scroll
 
-#класс платформы
+    def check_end_game(self): # упал ли игрок
+        if self.rect.bottom > SCREEN_HEIGHT:
+            return True
+
+def end_screen():
+    screen = pygame.display.set_mode((SCREEN_WINDTH, SCREEN_HEIGHT))
+    running = True
+    font = pygame.font.Font(None, 50)
+    text = font.render('Game over', True, 'red')
+    text_x = SCREEN_WINDTH // 2 - text.get_width() // 2
+    text_y = SCREEN_HEIGHT // 2 - text.get_height() // 2
+    screen.fill((0, 0, 0))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.blit(text, (text_x, text_y))
+
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
+    pygame.quit()
+
+
+# класс платформы
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, *group):
         super().__init__(*group)
@@ -102,14 +129,14 @@ sprite_platforms.add(platform)
 # Основной игровой цикл
 running = True
 while running:
+    # создание платформ
     if len(sprite_platforms) < 10:
         platform_width = 110
         platform_x = random.randint(0, SCREEN_WINDTH - platform_width)
         platform_y = platform.rect.y - random.randint(80, 120)
         platform = Platform(platform_x, platform_y)
         sprite_platforms.add(platform)
-    # Рисование заднего фона
-    # screen.blit(image_background, (0, 0))
+
     screen.fill((210, 210, 210))
     pygame.draw.line(screen, 'black', (0, SCROLL_TRIGGER), (SCREEN_WINDTH, SCROLL_TRIGGER))
 
@@ -120,6 +147,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if player.check_end_game(): # если игрок упал, то появляется экран с game over
+        end_screen()
+        running = False
+        pygame.quit()
 
     pygame.display.flip()
     clock.tick(FPS)
