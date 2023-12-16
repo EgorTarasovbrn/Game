b6b1e4f1bc3ca43eb90494962fc37a60f32598a9
Egor <EgorTarasovbrn@yadnex.ru>
2023-12-16 21:06:41 +0700

    просто сохранение

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,81 +7,121 @@ pygame.init()
 SCREEN_WINDTH = 600
 SCREEN_HEIGHT = 700
 
+# высота границы
+SCROLL_TRIGGER = 250
+
 # fps
 FPS = 60
 clock = pygame.time.Clock()
-
+# гравитация
 GRAVITY = 1
 
 # Создание окна
 screen = pygame.display.set_mode((SCREEN_WINDTH, SCREEN_HEIGHT))
+# название окна
 pygame.display.set_caption('Game')
 
 # Загрузка изображения(просто для примера)
-image_background = pygame.image.load('game_fon.jpg')
-image_person = pygame.image.load('person.png')
+image_background = pygame.image.load('game_fon.jpg')  # задний фон
+image_platform = pygame.image.load('data/platform.png')  # платформа
+
+image_person = pygame.image.load('data\player.png')  # игрок
+image_person_width = image_person.get_width()
+image_person_height = image_person.get_height()
+
+sprite_player = pygame.sprite.Group()
+sprite_platforms = pygame.sprite.Group()  # группа платформ
 
 
-class Player:
-    def __init__(self, x, y):
-        self.image = pygame.transform.scale(image_person, (100, 100))
-        self.image_width = self.image.get_width()
-        self.image_height = self.image.get_height()
-        self.coords = [x, y]
-        self.gravity = 10
+# класс игрока
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, *group):
+        super().__init__(*group)
+        self.image = pygame.transform.scale(image_person, (image_person_width * 2, image_person_height * 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.vel_y = 0
 
-    def draw(self):
-        screen.blit(self.image, (self.coords))
-
-    def move(self):
-        dx = 0
-        dy = 0
+    def move(self):  # движение игрока
+        dx = 0  # изменение координаты х
+        dy = 0  # изменение координаты y
+        scroll = 0
 
         key = pygame.key.get_pressed()  # список нажатых кнопок
-        if key[pygame.K_a]:
+        if key[pygame.K_a]:  # движение влево
             dx -= 10
-        if key[pygame.K_d]:
+        if key[pygame.K_d]:  # движение вправо
             dx += 10
 
-        self.vel_y += GRAVITY
+        self.vel_y += GRAVITY  # прыжок
         dy += self.vel_y
 
-        x, y = self.coords
-        if x < 0 - self.image.get_width():
-            self.coords[0] = SCREEN_WINDTH
-        elif x > SCREEN_WINDTH:
-            self.coords[0] = -self.image.get_width()
+        # проверка пересечения игроком границ карты слева и справа
+        if self.rect.x < 0 - self.rect.width:
+            self.rect.x = SCREEN_WINDTH
+        elif self.rect.x > SCREEN_WINDTH:
+            self.rect.x = -self.rect.width
+        # проверка запрыгнул ли игрок на платформу
+        for platform in sprite_platforms:
+            if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.rect.width, self.rect.height):
+                if self.rect.bottom < platform.rect.centery:
+                    if self.vel_y > 0:
+                        self.rect.bottom = platform.rect.top
+                        dy = 0
+                        self.vel_y = -20
+        # проверка коснулся ли игрок скролла или нет
+        if self.rect.y <= SCROLL_TRIGGER and self.vel_y < 0:
+            scroll = -dy
 
-        if y > SCREEN_HEIGHT - self.image_height:
-            self.dy = 0
-            self.vel_y = -25
+        self.rect.x += dx # изменение координаты х у игрока(движение)
+        self.rect.y += dy + scroll # изменение координаты y у игрока(движение)
+        return scroll
 
-        self.coords[0] += dx
-        self.coords[1] += dy
+#класс платформы
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, *group):
+        super().__init__(*group)
+        self.image = image_platform
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, scroll):
+        self.rect.y += scroll
+
+        if self.rect.y > SCREEN_HEIGHT:
+            self.kill()
 
 
-player = Player(SCREEN_WINDTH // 2 - 50, SCREEN_HEIGHT - 150)
+player = Player(SCREEN_WINDTH // 2 - image_person_width // 2, SCREEN_HEIGHT - 200)
+sprite_player.add(player)
+platform = Platform(SCREEN_WINDTH // 2 - image_person_width // 2, SCREEN_HEIGHT - 100)
+sprite_platforms.add(platform)
 
 # Основной игровой цикл
 running = True
 while running:
-
+    if len(sprite_platforms) < 10:
+        platform_width = 110
+        platform_x = random.randint(0, SCREEN_WINDTH - platform_width)
+        platform_y = platform.rect.y - random.randint(80, 120)
+        platform = Platform(platform_x, platform_y)
+        sprite_platforms.add(platform)
     # Рисование заднего фона
-    screen.blit(image_background, (0, 0))
+    # screen.blit(image_background, (0, 0))
+    screen.fill((210, 210, 210))
+    pygame.draw.line(screen, 'black', (0, SCROLL_TRIGGER), (SCREEN_WINDTH, SCROLL_TRIGGER))
 
-    # Рисование спрайтов
-    player.draw()
-
-    # Движение персонажа
-    player.move()
+    sprite_platforms.update(player.move())
+    sprite_player.draw(screen)
+    sprite_platforms.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     pygame.display.flip()
-
     clock.tick(FPS)
 
 pygame.quit()
