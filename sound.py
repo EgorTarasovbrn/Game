861737c27925e4dc5a3156dc5724a4df460fb606
mirrroxx <astakhov2547@yandex.ru>
2024-01-08 17:06:55 +0700

    добавил все звуки

--- sound.py
+++ sound.py
@@ -8,9 +8,20 @@ def hit_sound():
 
 def jump_sound():
     jump = pygame.mixer.Sound('data/jump.mp3')
+    jump.set_volume(0.3)
     jump.play()
 
 
 def throw_sound():
     throw = pygame.mixer.Sound('data/throw.mp3')
     throw.play()
+
+
+def fall_sound():
+    fall = pygame.mixer.Sound('data/fall.mp3')
+    fall.play()
+
+
+def hit2_sound():
+    hit2 = pygame.mixer.Sound('data/hit2.mp3')
+    hit2.play()
